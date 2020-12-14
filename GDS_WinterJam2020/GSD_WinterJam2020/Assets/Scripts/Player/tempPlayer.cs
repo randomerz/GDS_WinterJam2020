9e24c1a061226c5d212c0b55ee91ef8886714e82
Mikail Khan <mikail.khan45@gmail.com>
2020-12-14 18:27:00 -0500

    added dash

--- GDS_WinterJam2020/GSD_WinterJam2020/Assets/Scripts/Player/tempPlayer.cs
+++ GDS_WinterJam2020/GSD_WinterJam2020/Assets/Scripts/Player/tempPlayer.cs
@@ -2,30 +2,112 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum State {
+    Standing,
+    Running,
+    Dashing,
+}
+
+public class Timer {
+    public float timeRemaining;
+    private float startTime;
+
+    public Timer(float time) {
+        timeRemaining = 0.0f;
+        startTime = time;
+    }
+
+    public void update(float deltaTime) {
+        timeRemaining = Mathf.Max(timeRemaining - deltaTime, 0.0f);
+    }
+
+    public bool isFinished() {
+        return timeRemaining == 0.0f;
+    }
+
+    public bool isRunning() {
+        return timeRemaining > 0.0f;
+    }
+
+    public void reset() {
+        timeRemaining = startTime;
+    }
+}
+
 public class tempPlayer : MonoBehaviour
 {
     public float moveSpeed;
 
-
     public Animator playerAnim;
     public SpriteRenderer playerSprite;
 
-    private float hoMovement;
-    private float vertMovement;
-    private Vector3 moveDir;
+    private State state;
+
+    public float dashTime;
+    public float dashCooldown;
+    private Timer dashTimer;
+    private Timer dashCooldownTimer;
+
+    public float dashDamping = 0.95f;
+    public float runDamping = 0.5f;
+
+    public float dashMultiplier = 10.0f;
+
+    private Vector3 velocity = new Vector3(0.0f, 0.0f, 0.0f);
 
     void Start()
     {
-        
+        dashTimer = new Timer(dashTime);
+        dashCooldownTimer = new Timer(dashCooldown);
     }
-    
+
     void Update()
     {
-        hoMovement = Input.GetAxisRaw("Horizontal");
-        vertMovement = Input.GetAxisRaw("Vertical");
-        moveDir = new Vector3(hoMovement, vertMovement).normalized;
+        //Debug.Log(dashTimer.timeRemaining);
 
-        playerAnim.SetBool("isRunning", moveDir.magnitude > 0);
+        switch (state) {
+            case State.Running: velocity *= runDamping; break;
+            case State.Dashing: velocity *= dashDamping; break;
+            default: break;
+        }
+
+        /* velocity /= state switch { */
+        /*     State.Running => runDamping, */
+        /*     State.Dashing => dashDamping, */
+        /*     State.Standing => 1, */
+        /* }; */
+
+        if (state != State.Dashing) {
+            velocity.x += Input.GetAxisRaw("Horizontal") * moveSpeed;
+            velocity.y += Input.GetAxisRaw("Vertical") * moveSpeed;
+        }
+
+        if (velocity.magnitude <= 0.05f) {
+            velocity.x = 0.0f;
+            velocity.y = 0.0f;
+            velocity.z = 0.0f;
+        }
+
+        if (Input.GetKeyDown("space")) {
+            handleDash();
+        }
+
+        if (state == State.Dashing) {
+            dashTimer.update(Time.deltaTime);
+            if (dashTimer.isFinished()) {
+                setState(State.Standing);
+            }
+        }
+
+        if (state != State.Dashing && velocity.magnitude > 0) {
+            setState(State.Running);
+        } else if (state == State.Running && velocity.magnitude == 0) {
+            setState(State.Standing);
+        }
+
+        if (state == State.Running && velocity.magnitude > moveSpeed) {
+            velocity = velocity.normalized * moveSpeed;
+        }
 
         // mouse
         // Project the mouse point into world space at
@@ -41,8 +123,26 @@ public class tempPlayer : MonoBehaviour
         playerSprite.flipX = 90 < fAngle && fAngle < 270;
     }
 
+    void setState(State s) {
+        state = s;
+        playerAnim.SetBool("isRunning", state == State.Running);
+        //playerAnim.SetBool("isDashing", state == State.Dashing);
+    }
+
+    void handleDash() {
+        Debug.Log(dashCooldownTimer.timeRemaining);
+        if (state != State.Dashing && dashCooldownTimer.isFinished()) {
+            dashCooldownTimer.reset();
+            dashTimer.reset();
+            velocity *= dashMultiplier;
+            setState(State.Dashing);
+        }
+    }
+
     private void FixedUpdate()
     {
-        transform.position += moveSpeed * Time.deltaTime * moveDir;
+        dashTimer.update(Time.deltaTime);
+        dashCooldownTimer.update(Time.deltaTime);
+        transform.position += velocity;
     }
 }
