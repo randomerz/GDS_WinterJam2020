88aac260abea1ce1c3dac429f5dd6037df1eb1d5
boomo <randomerz56@gmail.com>
2020-12-14 16:14:05 -0500

    added file structure stuff and some player stuff

---
+++ GDS_WinterJam2020/GSD_WinterJam2020/Assets/Scripts/Player/tempPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tempPlayer : MonoBehaviour
+{
+    public float moveSpeed;
+
+
+    private float hoMovement;
+    private float vertMovement;
+    private Vector3 moveDir;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        hoMovement = Input.GetAxisRaw("Horizontal");
+        vertMovement = Input.GetAxisRaw("Vertical");
+        moveDir = new Vector3(hoMovement, vertMovement).normalized;
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position += moveSpeed * Time.deltaTime * moveDir;
+    }
+}
