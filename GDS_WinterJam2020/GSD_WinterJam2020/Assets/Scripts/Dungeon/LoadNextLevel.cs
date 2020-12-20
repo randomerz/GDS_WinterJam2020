3d67ddda1c86cdad7ca79a6323cb224e442984d1
boomo <randomerz56@gmail.com>
2020-12-20 02:35:41 -0500

    i forgot what i updated ooga booga

---
+++ GDS_WinterJam2020/GSD_WinterJam2020/Assets/Scripts/Dungeon/LoadNextLevel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadNextLevel : MonoBehaviour
+{
+    public string levelName = "";
+
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene(levelName);
+    }
+}
