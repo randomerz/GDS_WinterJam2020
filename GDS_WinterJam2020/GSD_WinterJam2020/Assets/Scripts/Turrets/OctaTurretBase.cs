71145a75a729becc25ab2bff11494664c27208c8
Mikail Khan <mikail.khan45@gmail.com>
2020-12-20 03:14:50 -0500

    fixed octaturret

--- GDS_WinterJam2020/GSD_WinterJam2020/Assets/Scripts/Turrets/OctaTurretBase.cs
+++ GDS_WinterJam2020/GSD_WinterJam2020/Assets/Scripts/Turrets/OctaTurretBase.cs
@@ -4,24 +4,20 @@ using UnityEngine;
 
 public class OctaTurretBase : TurretBase
 {
-    
-
-  
-
     void shoot() {
         // idk how this works
         fireTimer.reset();
-        GameObject target = aimAlgo.getTarget();
-        float angle = ((transform.rotation.eulerAngles.z + 90) * Mathf.Deg2Rad);
+        //GameObject target = aimAlgo.getTarget();
+        //float angle = ((transform.rotation.eulerAngles.z + 90) * Mathf.Deg2Rad);
         for (int i = 0; i < 8; i++)
         {
             Projectile p =
                 Instantiate(projectile, new Vector3(transform.position.x, transform.position.y, 0.5f), Quaternion.identity);
             p.setTeam(team);
 
-            float tmp_angle = angle+i*(Mathf.PI/4);
+            float tmp_angle = i*(Mathf.PI/4);
             Vector3 dir = new Vector3(Mathf.Cos(tmp_angle), Mathf.Sin(tmp_angle), 0.0f);
-            p.initialize(projectileSpeed, dir, projectileDamage, target);
+            p.initialize(projectileSpeed, dir, projectileDamage, null);
         }
 
         audioManager.PlayOneShotSFX(3);
